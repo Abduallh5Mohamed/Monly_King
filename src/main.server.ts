d81b052b55744f9a43a84b52f85568dbf943ebde
Abduallh Mohamed Abduallh <ai4231493@deltauniv.edu.eg>
2025-09-15 05:39:37 +0300

    Fix SSR bootstrap with correct lambda function and BootstrapContext

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,15 +1,20 @@
 import { renderApplication } from '@angular/platform-server';
-import { Provider } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Provider, BootstrapContext } from '@angular/core';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: { providers: Provider[] }) => renderApplication(App, {
-  appId: 'serverApp',
-  ...config,
-  providers: [
-    ...(config.providers || []),
-    ...(context.providers || [])
-  ]
-});
+const bootstrap = (context: { providers: Provider[] }) => renderApplication(
+  (bootstrapContext: BootstrapContext) => bootstrapApplication(App, {
+    ...config,
+    providers: [
+      ...(config.providers || []),
+      ...(context.providers || [])
+    ]
+  }),
+  {
+    appId: 'serverApp'
+  }
+);
 
 export default bootstrap;
